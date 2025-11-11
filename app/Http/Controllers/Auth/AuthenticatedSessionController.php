1a8c86c28414d28cd1622bb7849ee27a3ff081c6
s.kazmi <s.kazmi@metro-markets.de>
2025-11-11 18:39:39 +0100

    show self condition added

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -125,7 +125,7 @@ class AuthenticatedSessionController extends Controller
             // Find first user with helper or business role
             $user = User::whereHas('roles', function ($query) {
                 $query->whereIn('name', ['helper', 'business'])
-                ->whereNotIn('name', ['admin']);
+                ->whereNotIn('name', ['admin', 'super_admin']);
             })->orderBy('id')->first();
 
             if (!$user) {
