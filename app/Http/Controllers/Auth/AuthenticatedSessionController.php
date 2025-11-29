85e4914580e96354992dbde2a194e8aae90bdd39
s.kazmi <sydabdrehman@gmail.com>
2025-11-29 08:06:23 +0100

    worker added

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -123,7 +123,7 @@ class AuthenticatedSessionController extends Controller
 
         if ($normalizedPhone === $demoPhoneNumber || $normalizedPhone === '+' . $demoPhoneNumber) {
             // Find first user with ONLY business role
-            $user = User::find(11);
+            $user = User::find(14);
 
             if (!$user) {
                 // Fallback: Try to find any business user if strict check fails, or return error
