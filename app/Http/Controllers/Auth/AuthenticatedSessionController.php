0fe568f5dd6fd552224de38532751c48cbcab59d
s.kazmi <sydabdrehman@gmail.com>
2025-12-18 07:02:48 +0100

    kamwaalay app fixes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -124,7 +124,7 @@ class AuthenticatedSessionController extends Controller
 
         if ($normalizedPhone === $demoPhoneNumber || $normalizedPhone === '+' . $demoPhoneNumber) {
             // Find first user with ONLY business role
-            $user = User::find(4);
+            $user = User::find(6);
 
 
             if (!$user) {
