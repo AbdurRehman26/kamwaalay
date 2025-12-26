ee79cb6d4d1b3960d6c32e199d011548d65381ad
s.kazmi <sydabdrehman@gmail.com>
2025-12-26 13:51:01 +0100

    demo testing added

--- resources/js/Pages/Bookings/Create.jsx
+++ resources/js/Pages/Bookings/Create.jsx
@@ -168,21 +168,6 @@ export default function BookingCreate() {
                     </div>
                 ) : (
                     <div className="max-w-3xl mx-auto">
-                        {/* Notice about Karachi only */}
-                        <div className="bg-yellow-50 dark:bg-yellow-900/20 border-l-4 border-yellow-400 dark:border-yellow-500 mb-6 rounded-xl p-4">
-                            <div className="flex">
-                                <div className="flex-shrink-0">
-                                    <svg className="h-5 w-5 text-yellow-400 dark:text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-                                        <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                                    </svg>
-                                </div>
-                                <div className="ml-3">
-                                    <p className="text-sm text-yellow-700 dark:text-yellow-300">
-                                        <strong>Note:</strong> We are currently serving <strong>Karachi, Lahore, and Islamabad</strong>. More cities coming soon!
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
                         <form onSubmit={submit} className="bg-white dark:bg-gray-800 rounded-2xl shadow-xl p-8 border border-gray-100 dark:border-gray-700 relative z-20 -mt-10 space-y-6">
                             <div className="grid md:grid-cols-2 gap-6">
                                 <div>
